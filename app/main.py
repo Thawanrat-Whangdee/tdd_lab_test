f4dd501cd82002a17815da50619caa162327a125
Thawanrat Whangdee <117657807+Thawanrat-Whangdee@users.noreply.github.com>
2023-03-22 15:04:13 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -20,8 +20,8 @@ def read_item(item_id: int, q: str = None):
 def read_name(name: str = None):
     return {"hello": name}
 
-@app.post("/callname/")
-def create_name(name: Name):
+@app.post("/callname")
+def create_name(name: str = "thawanrat",name: Name):
    return {"hello": name}
 
 handler = Mangum(app)
